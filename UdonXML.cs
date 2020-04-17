c699beb9f50a8262337e904315d6acd9e67012f3
Foorack <max@foorack.com>
2020-04-18 01:18:16 +0200

    Prepare for release of v0.2.0

--- UdonXML.cs
+++ UdonXML.cs
@@ -30,6 +30,7 @@
  *         0.1.1: 2020-04-12; Added support for !DOCTYPE, thereby allowing HTML to be parsed.
  *         0.1.2: 2020-04-12; Many parsing issues fixed. Now possible to export XML data.
  *         0.1.3: 2020-04-12; Added ability to change values and create/delete attributes and child nodes.
+ *         0.2.0: 2020-04-18; Better handle HTML input, especially regarding comments and certain self-closing HTML tags.
  * 
  */
 
